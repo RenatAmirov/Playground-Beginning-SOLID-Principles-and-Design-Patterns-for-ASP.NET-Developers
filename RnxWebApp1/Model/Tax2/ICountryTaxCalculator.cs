378e72a2ae480a0aab45341938289f8a6eaee4d5
renat amirov <amirov.renat.1st@gmail.com>
2022-07-10 00:52:19 +0400

    Add Tax2 throw Interface.

---
+++ RnxWebApp1/Model/Tax2/ICountryTaxCalculator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RnxWebApp1.Model.Tax2
+{
+    public interface ICountryTaxCalculator
+    {
+        decimal CalculateTaxAmount();
+    }
+}
---
+++ RnxWebApp1/Model/Tax2/TaxCalculatorForUS2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RnxWebApp1.Model.Tax2
+{
+    public class TaxCalculatorForUS2: ICountryTaxCalculator
+    {
+        public decimal CalculateTaxAmount()
+        {
+            // TODO Logic
+            return 0;
+        }
+    }
+}
